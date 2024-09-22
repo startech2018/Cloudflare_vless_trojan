c1184a8ddbdcc5e6df0348bdf14bced7ae6f7b2d
startech2018 <startech@qq.com>
2024-09-22 18:04:49 +0800

    Update _worker.js

--- Trojan_workers_pages/_worker.js
+++ Trojan_workers_pages/_worker.js
@@ -1,8 +1,8 @@
 // src/worker.js
 import { connect } from "cloudflare:sockets";
  
-let Pswd = "trojan";
-const proxyIPs = ["proxy.xxxxxxxx.tk"]; //ts.hpc.tw edgetunnel.anycast.eu.org bestproxy.onecf.eu.org cdn-all.xn--b6gac.eu.org cdn.xn--b6gac.eu.org proxy.xxxxxxxx.tk
+let Pswd = "mskj.com";
+const proxyIPs = ["ts.hpc.tw"]; //ts.hpc.tw edgetunnel.anycast.eu.org bestproxy.onecf.eu.org cdn-all.xn--b6gac.eu.org cdn.xn--b6gac.eu.org proxy.xxxxxxxx.tk
 let cn_hostnames = [''];
 let CDNIP = 'www.visa.com.sg'
 // http_ip
